a8791077e58d6f8f00324aeb8fe65fce09abdc22
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-06-11 11:48:23 +0530

    Added basic Response

---
+++ src/main/java/com/ing/modelbank/pojo/CustomerDetaillsResponse.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ing.modelbank.pojo;
+
+import java.io.Serializable;
+
+/**
+ * Container for Customer Detaills Response
+ * @author ING Dev Team
+ *
+ */
+public class CustomerDetaillsResponse implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
