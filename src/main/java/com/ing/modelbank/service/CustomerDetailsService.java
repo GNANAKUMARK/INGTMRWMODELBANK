b7bd7156209d83475a6f310938519810fc7ca88e
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-06-11 12:21:51 +0530

    Customer Service

---
+++ src/main/java/com/ing/modelbank/service/CustomerDetailsService.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.ing.modelbank.service;
+
+/**
+ * Interface contract for CustomerDetailsService
+ * @author ING Dev Team
+ *
+ */
+public interface CustomerDetailsService {
+
+}
---
+++ src/main/java/com/ing/modelbank/service/CustomerDetailsServiceImpl.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ing.modelbank.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service implementation for Customer Details Service
+ * @author ING Dev team
+ *
+ */
+@Service
+public class CustomerDetailsServiceImpl implements CustomerDetailsService {
+
+}
