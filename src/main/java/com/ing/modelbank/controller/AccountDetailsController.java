010bff7fb02eb68fb09213cd1675860aa25403d0
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-06-11 11:34:20 +0530

    Added base controller

---
+++ src/main/java/com/ing/modelbank/controller/AccountDetailsController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.ing.modelbank.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for Account details
+ * @author ING Dev Team
+ *
+ */
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping(path = "/api")
+public class AccountDetailsController {
+	private static final Logger LOGGER = LogManager.getLogger(AccountDetailsController.class);
+	
+
+}
