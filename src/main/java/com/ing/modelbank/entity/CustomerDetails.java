01a6c7008e2ab4e305f333dbbb5db9a623d39eb0
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-06-11 12:36:17 +0530

    CusotmerDetails entity

--- src/main/java/com/ing/modelbank/entity/CustomerDetails.java
+++ src/main/java/com/ing/modelbank/entity/CustomerDetails.java
@@ -4,8 +4,16 @@
 package com.ing.modelbank.entity;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Container for  Customer Details entity
@@ -13,12 +21,30 @@ import javax.persistence.Entity;
  *
  */
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class CustomerDetails implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String accountType;
+	
+	private String customerName;
+	
+	private Double balance;
+	
+	private String customerId;
+	
+	private LocalDate creationDate;
+	
 	
 
 }
