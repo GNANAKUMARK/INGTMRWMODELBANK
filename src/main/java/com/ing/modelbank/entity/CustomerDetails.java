c437b5f4322d2ae9709ec9d5ebf7c08deb2e2a74
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-06-11 11:57:32 +0530

    added annotation to CustomerDetails entity.

--- src/main/java/com/ing/modelbank/entity/CustomerDetails.java
+++ src/main/java/com/ing/modelbank/entity/CustomerDetails.java
@@ -3,11 +3,21 @@
  */
 package com.ing.modelbank.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
 /**
  * Container for  Customer Details entity
  * @author ING Dev Team
  *
  */
-public class CustomerDetails {
+@Entity
+public class CustomerDetails implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
